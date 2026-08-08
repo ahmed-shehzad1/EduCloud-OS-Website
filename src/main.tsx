c4a758f34d47d0e87b57ad1beb3b67806a320749
Ahmed Shahzad <243330@students.au.edu.pk>
2026-08-08 15:37:32 +0500

    Auth layering done

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes/AppRoutes';
 import './styles/global.css';
+import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+     <AuthProvider>
       <AppRoutes />
+     </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
