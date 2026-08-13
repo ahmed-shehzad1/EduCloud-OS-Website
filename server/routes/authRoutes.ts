5dbca443c45bd749d8924ffa2e20be672c189d36
Ahmed Shahzad <243330@students.au.edu.pk>
2026-08-13 23:14:39 +0500

    fix: incorrect export issue

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,14 +1,19 @@
 import { Router } from 'express';
+
 import {
   redirectToGitHub,
   handleGitHubCallback,
+  getCurrentUser,
 } from '../controllers/authController';
+
 import { requireAuth } from '../middleware/authMiddleware';
 
 const router = Router();
 
 router.get('/github', redirectToGitHub);
+
 router.get('/github/callback', handleGitHubCallback);
+
 router.get('/me', requireAuth, getCurrentUser);
 
 export default router;
